c50fc05c1a0bc07ccdbe7e75003015cfe4b9c2c6
DESKTOP-1RROORF\johan <johanrahar@gmail.com>
2018-08-05 13:53:25 +0700

    venv init

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Dev\htdocs\knn-rec\venv"
+$env:VIRTUAL_ENV="E:\Xampp\htdocs\movie-rec\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=D:\Dev\htdocs\knn-rec\venv"
+set "VIRTUAL_ENV=E:\Xampp\htdocs\movie-rec\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
